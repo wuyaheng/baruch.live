53941ac2335093d992b3ee3023abca3b2c52a703
wuyaheng <wuyaheng2016@gmail.com>
2020-10-18 16:35:56 -0400

    add title

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,12 +12,13 @@ class Home extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-6 mt-4 pt-4 text-center">
+                            <h1>Baruch.Live</h1>
                             <h5 className="mt-4 pt-4">We provides a full view of all the current and upcoming events held at Baruch and open to Baruch students</h5>
 
                             <button type="button" class="btn btn-outline-dark mt-4">View Calendar</button>
                         </div>
                         <div className="col-md-6">
-                            <img src="https://cdn.glitch.com/c0dcb651-5b19-4756-a6e6-25b3982ca367%2Fblue-calendar-icon-15.png?v=1603051831012"/>
+                            <img src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2Fimage-vector.svg?v=1603053176910"/>
                         </div>
                     </div>
                 </div>
