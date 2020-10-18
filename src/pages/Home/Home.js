ee879be91bf861ab279e39ec38e10a202bbd8ac4
wuyaheng <wuyaheng2016@gmail.com>
2020-10-18 16:19:36 -0400

    add langing page text and button

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,6 +8,19 @@ class Home extends Component {
             <div class="alert alert-primary" role="alert">
                <h4 className="text-center">Baruch.Live</h4>
                 </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 mt-4 pt-4 text-center">
+                            <h5 className="mt-4 pt-4">We provides a full view of all the current and upcoming events held at Baruch and open to Baruch students</h5>
+
+                            <button type="button" class="btn btn-outline-dark mt-4">View Calendar</button>
+                        </div>
+                        <div className="col-md-6">
+                            <img src="https://cdn.glitch.com/c0dcb651-5b19-4756-a6e6-25b3982ca367%2Fblue-calendar-icon-15.png?v=1603051831012"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
