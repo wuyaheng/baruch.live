94f4f5e014718da13f163338a9ba52ac2ed487f8
wuyaheng <wuyaheng2016@gmail.com>
2020-10-18 18:54:17 -0400

    add button style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
                     <nav className="nav-wrapper">
                     <div className="container">
                         <a href="#" className="sidenav-trigger" data-target="mobile-links">
-                            <i className="material-icons">menu</i>
+                            <i className="material-icons" style={{color: "#212529"}}>menu</i>
                         </a>
                         <ul className="right hide-on-med-and-down">
                             <li><a href="/">Home</a></li> 
@@ -24,15 +24,15 @@ class Home extends Component {
                 </ul>
 
                 <div className="container">
-                    <div className="row">
+                    <div className="row mt-4 pt-4">
                         <div className="col-md-6 text-center" id="landing-text">
                             <h1>Baruch.Live</h1>
                             <h6>We provides a full view of all the current and upcoming events held at Baruch and open to Baruch students</h6>
 
-                            <button type="button" class="btn btn-outline-dark mt-4">View Calendar</button>
+                            <button type="button" className="btn btn-outline-dark mt-4">View Calendar</button>
                         </div>
-                        <div className="col-md-6">
-                            <img src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2Fimage-vector.svg?v=1603053176910"/>
+                        <div className="col-md-6 text-center">
+                            <img width="80%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2Fimage-vector.svg?v=1603053176910"/>
                         </div>
                     </div>
                 </div>
