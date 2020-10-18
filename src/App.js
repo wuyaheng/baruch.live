7ae290bf455d0020f1ffb071542ea082af924e79
wuyaheng <wuyaheng2016@gmail.com>
2020-10-18 13:18:44 -0400

    add two default events

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Scheduler from './components/Scheduler';
 import './App.css';
  
 const data = [
-    { start_date:'2020-06-10 6:00', end_date:'2020-06-10 8:00', text:'Event 1', id: 1 },
-    { start_date:'2020-06-13 10:00', end_date:'2020-06-13 18:00', text:'Event 2', id: 2 }
+    { start_date:'2020-10-29 12:30', end_date:'2020-10-29 13:15', text:'Bloomberg Excel Add-In workshop (Register Link: https://www.baruch.cuny.edu/calendar/EventRegistration.aspx?Rid=3341&Iid=1121732&Frm=)', id: 1 },
+    { start_date:'2020-10-20 15:30', end_date:'2020-10-20 17:00', text:'Tools for Clear Speech workshop - Describing Line Graphs (Register Link: https://tfcs.mywconline.com/schedule.php?scheduleid=sc5f16d957565b5)', id: 2 }
 ];
  
 class App extends Component {
