8ef7f82303b0ac80313aea4edf72ab9b02ccc83f
wuyaheng <wuyaheng2016@gmail.com>
2020-11-19 19:30:00 -0500

    add team pic hover effect

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,14 +1,33 @@
-import React from 'react'
-
+import React from 'react';
+import styles from './Team.module.css';
 
 
 
 const Team = () => (
     <>
-   <div className="col-md-2 text-center"><img alt="Jing" width="90%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2FJingWang.png?v=1603066917833"/><h6 className="text-center">Jing Wang</h6></div>
-   <div className="col-md-2 text-center"><img alt="Sichan" width="90%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2FSichanChen.png?v=1603066923278"/><h6 className="text-center">Sichan Chen</h6></div>
-   <div className="col-md-2 text-center"><img alt="Yajie" width="90%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2FYajieHou.png?v=1603066925778"/><h6 className="text-center">Yajia Hou</h6></div>
-   <div className="col-md-2 text-center"><img alt="Phoebe" width="90%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2FYahengWu.png?v=1603066913331"/><h6 className="text-center">Yaheng Wu</h6></div>
+   <div className={styles.teammember} className="col-md-2 text-center">
+    <img className={styles.teamphoto} alt="Jing" width="90%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2FJingWang.png?v=1603066917833"/>
+    <h4 className="text-center" style={{color: "#5394A0", letterSpacing: "-1px"}}>Jing Wang</h4>
+    <p>Business Analyst</p>
+    </div>
+
+   <div className={styles.teammember} className="col-md-2 text-center">
+   <img className={styles.teamphoto} alt="Sichan" width="90%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2FSichanChen.png?v=1603066923278"/>
+   <h4 className="text-center" style={{color: "#5394A0", letterSpacing: "-1px"}}>Sichan Chen</h4> 
+   <p>System Designer</p>
+   </div>
+
+   <div className={styles.teammember} className="col-md-2 text-center">
+   <img className={styles.teamphoto} alt="Yajie" width="90%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2FYajieHou.png?v=1603066925778"/>
+   <h4 className="text-center" style={{color: "#5394A0", letterSpacing: "-1px"}}>Yajia Hou</h4>
+   <p>Product Manager</p> 
+   </div>
+
+   <div className={styles.teammember} className="col-md-2 text-center">
+   <img className={styles.teamphoto} alt="Phoebe" width="90%" src="https://cdn.glitch.com/8c639e73-4683-4cc1-b093-ef7b7328e9e5%2FYahengWu.png?v=1603066913331"/>
+   <h4 className="text-center" style={{color: "#5394A0", letterSpacing: "-1px"}}>Yaheng Wu</h4>
+   <p>Web Developer</p>
+   </div>  
  </>
 )
 
