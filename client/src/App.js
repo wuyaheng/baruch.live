1a22b872d66938218cbb2858ea70f2d7b92091b6
wuyaheng <wuyaheng2016@gmail.com>
2020-11-20 21:14:32 -0500

    update about

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home";
-import Calendar from "./pages/Calendar/Calendar";
-import About from "./pages/About/About";
+import Calendar from "./pages/Calendar/Calendar"; 
+import About from "./pages/About/About"
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register";
 
